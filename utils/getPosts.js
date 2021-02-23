193457d8b4174ea5e1e74a0c2fb95a03d3565368
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2021-02-23 22:20:29 +0000

    does this fix dates on blog posts?

--- utils/getPosts.js
+++ utils/getPosts.js
@@ -4,13 +4,20 @@ const getPosts = (context) => {
     const keys = context.keys()
     const values = keys.map(context)
 
+    const dateOptions = {
+        day: '2-digit', month: 'long', year: 'numeric'
+    }
+    const dateTimeFormat = new Intl.DateTimeFormat('en-GB', dateOptions);
+
     const data = keys.map((key, index) => {
         let slug = `/${key.replace(/^.*[\\\/]/, '').slice(0, -3)}`
         const value = values[index]
         const postData = matter(value.default)
+
+        const postDate = new Date(postData.data.data)
         return {
             title: postData.data.title,
-            date: new Intl.DateTimeFormat('en-GB', { day: '2-digit', month: 'long', year: 'numeric' }).format(postData.data.date),
+            date: dateTimeFormat.format(postDate),
             markdownBody: postData.content,
             featured: postData.data.featured || false,
             slug,
