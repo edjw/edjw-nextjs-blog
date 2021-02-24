0e7117f2f68dc965fc2995a34f5240c4dc905d0b
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2021-02-24 13:14:59 +0000

    add headers

--- next.config.js
+++ next.config.js
@@ -7,11 +7,33 @@ module.exports = {
                 test: /\.md$/,
                 use: 'raw-loader',
             },
-            // {
-            //     test: /\.md$/,
-            //     use: 'file-loader'
-            // }
         )
         return config
+    },
+    async headers() {
+        return [
+            {
+                source: '/',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: ' X-XSS-Protection',
+                        value: '1; mode=block',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'same-origin',
+                    },
+
+                ],
+            },
+        ]
     },
 }
