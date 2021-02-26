4f44036b974457e1ea15839e9eba13ef8627e295
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2021-02-26 12:55:12 +0000

    rss feed

--- pages/feed.xml.js
+++ pages/feed.xml.js
@@ -1,39 +1,36 @@
 import allPosts from '../data/allBlogposts'
 import siteData from '../data/siteconfig'
-const { url: siteURL, title: siteTitle, description: siteDescription } = siteData
+const { url: siteURL, title: siteTitle, description: siteDescription, author: { name } } = siteData
 import markdownToHtml from '../utils/md2HTML'
 
 async function generateRss(posts) {
-    const itemsList = await Promise.all(allPosts.map(generateRssItem))
+  const itemsList = await Promise.all(allPosts.map(generateRssItem))
 
-    return `
-    <rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" version="2.0">
-      <channel>
-        <title>${siteTitle}</title>
-        <link>${siteURL}</link>
-        <description>${siteDescription}</description>
-        <language>en</language>
-        <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
-        <atom:link href="${siteURL}/feed.xml" rel="self" type="application/rss+xml"/>
-
-        ${itemsList.join('')}
-
-        </channel>
-    </rss>
+  return `<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+      <title>${siteTitle}</title>
+      <link href="${siteURL}" rel="self"/>
+      <description>${siteDescription}</description>
+      <language>en</language>
+      <updated>${new Date(posts[0].date).toISOString()}</updated>
+      <author>
+        <name>${name}</name>
+      </author>
+      ${itemsList.join('')}
+</feed>
   `
 }
 
 async function generateRssItem(post) {
-    const content = await markdownToHtml(post.markdownBody || '')
+  const content = await markdownToHtml(post.markdownBody || '')
 
-    return `
-    <item>
-      <guid>${siteURL}/posts${post.slug}</guid>
+  return `
+    <entry>
       <title>${post.title}</title>
-      <link>${siteURL}/posts/${post.slug}</link>
-      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-      <content type="html">${content}</content>
-    </item>
+      <link href="${siteURL}/posts/${post.slug}"/>
+      <updated>${new Date(post.date).toUTCString()}</updated>
+      <content type="html"><![CDATA[${content}]]></content>
+    </entry>
   `
 }
 
@@ -41,7 +38,7 @@ export default function RSSFeed() { };
 
 
 RSSFeed.getInitialProps = async ({ res }) => {
-    res.setHeader('Content-Type', 'text/xml');
-    res.write(await generateRss(allPosts));
-    res.end();
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(await generateRss(allPosts));
+  res.end();
 }
