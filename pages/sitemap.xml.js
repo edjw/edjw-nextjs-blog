7399a68e12cfc81b4ed3c43e2d3b6912d37d185f
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2021-02-26 12:40:25 +0000

    add lastmod to sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,16 +1,22 @@
+const { DateTime } = require("luxon");
 import allPosts from '../data/allBlogposts'
 import siteData from '../data/siteconfig'
 const { url: siteURL } = siteData
 
+const formatDate = (date) => {
+    return DateTime.fromFormat(date, 'dd MMMM yyyy').toFormat("yyyy-LL-dd");
+}
+
 const createSitemap = (allPosts) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${allPosts
-        .map(({ slug }) => {
+        .map(({ slug, date }) => {
             return `
-                    <url>
-                        <loc>${`${siteURL}${slug}`}</loc>
-                    </url>
-                `;
+        <url>
+            <loc>${`${siteURL}${slug}`}</loc>
+            <lastmod>${formatDate(date)}</lastmod>
+        </url>
+        `;
         })
         .join('')}
     </urlset>
