c1fdb78332f1c781cec9127ba1debcff1d7c251d
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2021-02-26 11:32:30 +0000

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import Layout from '../components/Layout'
+
+const title = "Sorry, that page doesn't exist"
+
+export default function FourOhFour() {
+    return (
+        <>
+            <Layout pageTitle={title} description={title}>
+
+                <h2>{title}</h2>
+
+                <p>That link doesn't work and it's probably my fault. Sorry about that.</p>
+
+                <p>Hopefully you can find what you're looking with one of these links below.</p>
+
+                <p>
+                    <Link href="/">
+                        <a>Go to the homepage</a>
+                    </Link>
+                </p>
+
+                <p>
+                    <Link href="/blog">
+                        <a>See a list of all blogposts</a>
+                    </Link>
+                </p>
+
+                <p>
+                    <Link href="/tags">
+                        <a>See a list of all blogpost tags</a>
+                    </Link>
+                </p>
+
+                <p>
+                    <Link href="/reading-list">
+                        <a>See my reading lists</a>
+                    </Link>
+                </p>
+            </Layout>
+        </>
+    )
+}
