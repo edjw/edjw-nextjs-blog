cf3575d86a3fbf1b40aa2901683a5f142d28e087
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2021-03-02 20:08:22 +0000

    take the renderscripts thing out of _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,15 +9,16 @@ class MyDocument extends Document {
 
     render() {
 
-        const shouldRenderScripts =
-            this.props.unstable_runtimeJS || process.env.NODE_ENV !== 'production'
+        // const shouldRenderScripts =
+        //     this.props.unstable_runtimeJS || process.env.NODE_ENV !== 'production'
 
         return (
             <Html lang="en">
                 <Head />
                 <body className='dark:bg-trueGray-800'>
                     <Main />
-                    {shouldRenderScripts ? <NextScript /> : null}
+                    <NextScript />
+                    {/* {shouldRenderScripts ? <NextScript /> : null} */}
                 </body>
             </Html>
         )
