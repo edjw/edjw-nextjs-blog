d1d64a1efa992d815b7c33c2e7eb7e189ee944d6
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2021-03-02 16:41:43 +0000

    fix up nav a bit

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -11,10 +11,10 @@ interface navItem {
 export default function Navigation() {
 
     return (
-        <nav className="grid grid-cols-3 mt-8 mb-4 prose list-none sm:w-1/3 gap-y-4 gap-x-1 dark:prose-light">
+        <nav className="grid w-9/12 grid-cols-3 mt-8 mb-4 prose list-none sm:w-6/12 md:w-6/12 gap-y-2 dark:prose-light">
             {navigation.map(({ url, text, attribute }: navItem, index) => (
                 <Link href={url} key={index}>
-                    <a className="p-1 text-lg transition duration-300 ease-in-out border-b-2 border-transparent min-w-min hover:border-yellow-200 hover:bg-yellow-100 hover:bg-opacity-20 dark:hover:border-gray-800 dark:hover:bg-yellow-200 dark:hover:bg-opacity-100" rel={attribute}>{text}</a>
+                    <a className="w-full p-1 text-lg transition duration-300 ease-in-out border-b-2 border-transparent hover:border-yellow-200 hover:bg-yellow-100 hover:bg-opacity-20 dark:hover:border-gray-800 dark:hover:bg-yellow-200 dark:hover:bg-opacity-100" rel={attribute}>{text}</a>
                 </Link>
             ))}
 
