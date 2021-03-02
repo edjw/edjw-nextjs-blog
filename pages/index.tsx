3123aa9540f5383acaa24e86337bc28585cc2dea
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2021-03-02 16:31:06 +0000

    turn off netlify identity widget

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Index({ featuredPosts, ...props }) {
     <>
       <Layout pageTitle={title} description={description}>
         <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+          {/* <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script> */}
           <link rel="authorization_endpoint" href="https://indieauth.com/auth" />
           <link rel="token_endpoint" href="https://tokens.indieauth.com/token" />
           <link rel="microsub" href="https://aperture.p3k.io/microsub/134" />
